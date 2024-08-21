97ad40612aef3288f2357fb273579deada654592
BryanWho <brayanromeroariza@gmail.com>
2024-08-21 18:47:35 -0500

    Probando repositorio desde terminal

--- src/main/java/com/danielsanchez/calculadorajava/Calculadora.java
+++ src/main/java/com/danielsanchez/calculadorajava/Calculadora.java
@@ -28,7 +28,14 @@ public class Calculadora {
                 break;
             
             // TODO: DEBES CREAR AQUÍ LOS CASES PARA LA OPERACIÓN DE LA POTENCIA Y EL MÓDULO.
-                
+            case '^':
+                respuesta = Math.pow(numero1, numero2);
+                break;
+
+            case '%':
+                respuesta = numero1 % numero2;
+                break;
+
             default:
                 throw new IllegalArgumentException("Operación inválida.");
         }
@@ -46,7 +53,7 @@ public class Calculadora {
         
         // Lectura de la operación a realizar
         // TODO: DEBES ADICIONAR EL SÍMBOLO DE ^ Y % PARA CALCULAR LA POTENCIA Y EL MÓDULO
-        System.out.println("Ingrese la operación (+, -, *, /)");
+        System.out.println("Ingrese la operación (+, -, *, /, ^, %)");
         char operacion = lector.next().charAt(0);
         
         double respuesta = calcular(numero1, numero2, operacion);
